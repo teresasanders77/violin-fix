505453ae984e93bf5261bb3dea2b0cf9ca7b1fa3
teresasanders77 <66372353+teresasanders77@users.noreply.github.com>
2021-09-01 17:38:48 -0600

    Added ACF field for video text

--- app/public/wp-content/themes/violin-fix/page-templates/home.php
+++ app/public/wp-content/themes/violin-fix/page-templates/home.php
@@ -13,6 +13,17 @@ get_header();
     <div class="entry-content">
       <h1>Welcome to the Premier Season!</h1>
       <?php the_content(); ?>
+      <div class="weekly-video-data">
+        <?php
+        /* ACF Section */
+        $composer = get_field('composer');
+        $performers = get_field('performers');
+        $piece = get_field('piece');
+        ?>
+        <p><strong>Composer: <?php echo $composer ?></strong></p>
+        <p><strong>Performers: <?php echo $performers ?></strong></p>
+        <p><strong>Piece: <?php echo $piece ?></strong></p>
+      </div>
     </div><!-- .entry-content -->
   </div>
 
